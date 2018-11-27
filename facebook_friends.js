61499dd7a2a3231c4875889f2968b861c2b23552
Tan5en5 <Tan5en5@users.noreply.github.com>
2018-11-27 19:32:50 +0100

    First commit
    This is the first commit of the facebook_friends.js

---
+++ facebook_friends.js
@@ -0,0 +1,19 @@
+lines = [];
+document.querySelectorAll('#pagelet_timeline_medley_friends div[data-testid="friend_list_item"]').forEach((nodeEl) => {
+    if (nodeEl.firstChild.attributes && 
+        nodeEl.firstChild.attributes['data-hovercard'] && 
+        nodeEl.firstChild.firstChild && 
+        nodeEl.firstChild.firstChild.attributes['aria-label']
+       ) {
+        name = nodeEl.firstChild.firstChild.attributes['aria-label'].nodeValue;
+        id = nodeEl.firstChild.attributes['data-hovercard'].nodeValue.match(/id=\d+/g)[0].substr(3);
+        lines.push(name+';'+id);
+    }
+});
+
+var reader = new FileReader();
+var out = new Blob([lines.join("\n")], {type: 'text/csv'});
+reader.onload = function(e){
+  window.location.href = reader.result;
+}
+reader.readAsDataURL(out);
